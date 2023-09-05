1e9f52d4313c9fecbd33af384b4b31f9bcf50871
claraalbuquerque <141173220+claraalbuquerque@users.noreply.github.com>
2023-09-05 15:02:17 -0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+numero= 10 
+
+nome = "joão"
+
+booleando = True
+
+a=10
+b=5
+soma = a + b
+subtração = a - b
+multiplicacao = a * b 
+divisao = a / b 
+
+va = True 
+vb = false 
+
+r1 = va and vb 
+r2 = va or vb
+r3 = not va
+
+c = 3 
+
+r4 = c > 4
+r5 = c == 4 
+r6 = c != 5 
+
+r7 = c > 1 and nome == "joão"
+
+
+
