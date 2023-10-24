8e325533b5ea9941736bc93de75ddb24f1fc1547
claraalbuquerque <141173220+claraalbuquerque@users.noreply.github.com>
2023-10-24 14:45:03 -0300

    Add files via upload

---
+++ programaçao.py
@@ -0,0 +1,32 @@
+def soma ( x, y) : 
+    return x + y 
+
+print ( soma ( 3, 5))
+
+print ( soma ( 2, 4))
+print ( soma ( 22, 7))
+print ( soma ( 1010, 2020))
+
+def simples (cor): 
+    if cor =='rosa':
+        return 'escolheu certo'
+    
+def medio (cor): 
+   if cor =='rosa': 
+        return 'escolheu certo'
+   else: 
+       return 'tente outra cor' 
+   
+def completo (cor): 
+    if cor == 'rosa': 
+        return 'escolheu certo'
+    elif cor == 'marrom': 
+        return 'não tem salvação'
+    else: 
+        return 'tente outra cor'
+    
+        
+        
+        
+      
+    
